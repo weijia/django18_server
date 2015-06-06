7c95e6d4eabf545e27ec1152a6cc3f418efe8ad9
Richard <richardwangwang@gmail.com>
2015-06-06 11:48:45 +0800

    Basic server structure created.

--- manage.py
+++ manage.py
@@ -2,9 +2,34 @@
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django18_server.settings")
 
+base_package_folder_name = "base_packages"
+libtool_relative_path = base_package_folder_name +"/libtool"
+external_app_folder_name = "external_app_repos"
+base_setting_folder_name = "django18_server"
+
+
+my_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(my_path, libtool_relative_path))
+from libtool import include_all, get_folder
+include_all(__file__, base_package_folder_name)
+include_all(__file__, external_app_folder_name)
+
+
+def initialize_settings():
+    from djangoautoconf import DjangoAutoConf
+    c = DjangoAutoConf()
+    c.set_base_extra_settings_list([])
+    c.set_external_app_folder_name("external_app_repos")
+    c.set_default_settings(base_setting_folder_name + ".settings")
+    c.set_root_dir(get_folder(__file__))
+    c.add_extra_settings_from_folder()
+    c.configure()
+
+
+if __name__ == "__main__":
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django18_server.settings")
+    initialize_settings()
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
